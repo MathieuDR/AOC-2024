9d77b5abddcaca0060cb6016f31554ca64f5165c
MathieuDR <mathieu@deraedt.dev>
2024-12-12 21:24:43 +0100

    ✨ Day 9 Part 1

--- lib/aoc_2024/solutions/y24/day09.ex
+++ lib/aoc_2024/solutions/y24/day09.ex
@@ -33,11 +33,61 @@ defmodule Aoc2024.Solutions.Y24.Day09 do
     ])
   end
 
-  def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
+  def part_one(blocks) do
+    size = Enum.reduce(blocks, 0, &(&2 + &1.file_size + &1.empty_size))
 
-    problem
+    0..(size - 1)
+    |> Enum.reduce_while({[], blocks}, fn
+      idx, {acc, blocks} ->
+        with {value, blocks} <- pop(blocks) do
+          {:cont, {[%{id: idx, value: value} | acc], blocks}}
+        else
+          nil ->
+            IO.puts("Halted at IDX: #{idx}")
+            {:halt, acc}
+        end
+    end)
+    |> Enum.reduce(0, &(&2 + &1.id * &1.value))
+  end
+
+  def pop([]), do: nil
+  def pop([%__MODULE__.Block{file_size: 0, empty_size: 0} | rest]), do: pop(rest)
+
+  def pop([%__MODULE__.Block{file_size: 0, empty_size: x} = current_block | rest]) do
+    current_block = %__MODULE__.Block{current_block | empty_size: x - 1}
+    {val, blocks} = pop_back(rest)
+
+    {val, [current_block | blocks]}
+  end
+
+  def pop([%__MODULE__.Block{file_size: x, id: id} = current_block | rest]) do
+    current_block = %__MODULE__.Block{current_block | file_size: x - 1}
+
+    {id, [current_block | rest]}
+  end
+
+  def pop_back(blocks) do
+    # If I need to take more then 1, because empty, this will be better then 2x last?
+    reversed = Enum.reverse(blocks)
+
+    idx = Enum.find_index(reversed, &(&1.file_size > 0))
+
+    case idx do
+      nil ->
+        {0, []}
+
+      _ ->
+        [%__MODULE__.Block{id: value, file_size: x} = current_block | rest] =
+          Enum.drop(reversed, idx)
+
+        current_block = %__MODULE__.Block{current_block | file_size: x - 1}
+
+        leftover =
+          [current_block | rest]
+          |> Enum.reverse()
+
+        {value, leftover}
+    end
   end
 
   # def part_two(problem) do
