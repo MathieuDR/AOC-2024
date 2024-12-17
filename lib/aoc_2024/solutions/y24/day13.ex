fcc518d6095d098c4d5f9bca4ab18ab63d6c881e
MathieuDR <mathieu@deraedt.dev>
2024-12-17 23:27:00 +0100

    📝 Lets pretend I understand this

--- lib/aoc_2024/solutions/y24/day13.ex
+++ lib/aoc_2024/solutions/y24/day13.ex
@@ -1,4 +1,75 @@
 defmodule Aoc2024.Solutions.Y24.Day13 do
+  @moduledoc """
+  # Linear Diophantine equation with 2 variables
+
+  I tried the [diophantine equation](https://mathworld.wolfram.com/DiophantineEquation.html) first, to give all solutions, especially working with the extended gdc
+
+  The math went like this
+  ```
+  Px = jAx + iBx
+  Py = jAy + iBy
+
+  Works for both X and Y, but lets take X as an example
+  {gcd, s, t} = Integer.extended_gcd(Ax, Bx)
+  if Px % gcd != 0 -> no solution
+  gcdFactor = Px / gcd
+  n = s * gcdFactor
+  m = t * gcdFactor
+  One possible solution is Px = nAx + mBx
+  This will most likely result into n / m that is not above 0
+  Since we calculate all solutions, it starts with negative ones we need a factor *k* to scale our solution
+  To get the k factor we can use the following formula to get around one of the tipping point
+  round( m / (a/gcd))
+  Or for the other one
+  round( n / (b/gcd))
+  To do this, we can use the following equation
+  j = n + k * (Bx / gcd)
+  i = m - k * (Ax / gcd)
+  ```
+
+  # Cramer's rule
+
+  Looking on the internet I found an interesting page that I only partially get [Cramer's rule](https://en.wikipedia.org/wiki/Cramer%27s_rule)
+
+  > In linear algebra, Cramer's rule is an explicit formula for the solution of a system of linear equations with as many equations as unknowns, valid whenever the system has a unique solution.
+  The worrying part is the *unique* solution. What if there are multiple solutions?
+
+  It uses matrices, so we need to rewrite our equations from before in the following form
+  See the [Applications section](https://en.wikipedia.org/wiki/Cramer%27s_rule#Applications) for it written in nice math.
+
+  ```
+  [Ax Bx][j] = [Px]
+  [Ay By][i]   [Py]
+  ```
+
+  ## Determinants
+  Cramer's rule uses [determinants](https://en.wikipedia.org/wiki/Determinant). Lets try to decipher what these are:
+  It's basically a constant that we calculate using the diagonals of a square matrix.
+
+  It can tell us the following: If the determinant is 0, we don't have a solution. If the determinant is not 0, we have exactly one solution and Cramer's rule will work.
+
+  Our determinants are
+
+  ```
+  AxBy - BxAy
+  ``` 
+
+  ## Putting it together
+
+  ```
+      [Px  Bx]
+      [Py  By]     PxBy - BxPy
+  x = --------  =  -----------
+      [Ax  Bx]     AxBy - BxAy
+      [Ay  By]
+
+      [Ax  Px]
+      [Ay  Py]     AxPy - PxAy
+  y = --------  =  -----------
+      [Ax  Bx]     AxBy - BxAy
+      [Ay  By]
+  ```
+  """
   alias AoC.Input
   alias Aoc2024.Helpers.Coords
 
@@ -54,11 +125,15 @@ defmodule Aoc2024.Solutions.Y24.Day13 do
   end
 
   def solve_machine(%ClawMachine{prize: prize, a_button: a, b_button: b}) do
-    x = diophantine_equation(prize.x, a.x, b.x)
-    y = diophantine_equation(prize.y, a.y, b.y)
+    diophantine_equation(prize.x - prize.y, a.x - a.y, b.x - b.y)
+    |> Enum.take(5)
+    |> IO.inspect()
 
-    match_streams(x, y)
-    |> Enum.take(1)
+    # x = diophantine_equation(prize.x, a.x, b.x)
+    # y = diophantine_equation(prize.y, a.y, b.y)
+    #
+    # match_streams(x, y)
+    # |> Enum.take(1)
   end
 
   def match_streams(stream_a, stream_b) do
@@ -107,14 +182,17 @@ defmodule Aoc2024.Solutions.Y24.Day13 do
       # k = Kernel.round(n / div(b, gcd))
 
       a_stream =
-        0..10000
+        0..100
         |> Stream.map(fn i -> n + s_add.(k - i) end)
 
       b_stream =
-        0..10000
+        0..100
         |> Stream.map(fn i -> m - t_add.(k - i) end)
 
       Stream.zip(a_stream, b_stream)
+      |> Stream.reject(fn {a, b} ->
+        a <= 0 or b <= 0
+      end)
     else
       []
     end
