178686daeeac52f5b3c3dea8dc8e5212d807126f
MathieuDR <mathieu@deraedt.dev>
2025-01-06 12:57:07 +0100

    🚧 Brute force attempt

--- lib/aoc_2024/solutions/y24/day17.ex
+++ lib/aoc_2024/solutions/y24/day17.ex
@@ -8,12 +8,12 @@ defmodule Aoc2024.Solutions.Y24.Day17 do
 
     registers = parse_registers(registers)
 
-    program =
+    {raw, parsed} =
       String.split(program)
       |> List.last()
       |> parse_program()
 
-    create_state(registers, program)
+    create_state(registers, parsed, raw)
   end
 
   def parse_registers(registers) do
@@ -27,14 +27,19 @@ defmodule Aoc2024.Solutions.Y24.Day17 do
   end
 
   def parse_program(program_string) do
-    String.split(program_string, ",", trim: true)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2, 2, :discard)
-    |> Enum.with_index(fn elem, i -> {i * 2, elem} end)
+    program =
+      String.split(program_string, ",", trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    parsed =
+      Enum.chunk_every(program, 2, 2, :discard)
+      |> Enum.with_index(fn elem, i -> {i * 2, elem} end)
+
+    {program, parsed}
   end
 
-  def create_state([a, b, c], program),
-    do: %{pointer: 0, output: [], A: a, B: b, C: c, program: Map.new(program)}
+  def create_state([a, b, c], program, raw),
+    do: %{pointer: 0, output: [], A: a, B: b, C: c, program: Map.new(program), raw: raw}
 
   def part_one(state) do
     %{output: solution} = __MODULE__.Computer.run_program(state)
@@ -43,7 +48,30 @@ defmodule Aoc2024.Solutions.Y24.Day17 do
     |> Enum.join(",")
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two(%{raw: raw} = state) do
+    desired = Enum.reverse(raw)
+
+    Stream.from_index()
+    |> Stream.transform(false, fn
+      _a, true ->
+        {:halt, nil}
+
+      a, false ->
+        if Integer.mod(a, 1_000_000) == 0 do
+          IO.puts("A: #{div(a, 1_000_000)}M")
+        end
+
+        %{output: s} =
+          %{state | A: a}
+          |> __MODULE__.Computer.run_program()
+
+        if s == desired do
+          {[a], true}
+        else
+          {[], false}
+        end
+    end)
+    |> Enum.to_list()
+    |> List.last()
+  end
 end
--- test/2024/day17_test.exs
+++ test/2024/day17_test.exs
@@ -29,38 +29,35 @@ defmodule Aoc2024.Solutions.Y24.Day17Test do
     apply(Solution, part, [problem])
   end
 
+  @input ~S"""
+  Register A: 729
+  Register B: 0
+  Register C: 0
+
+  Program: 0,1,5,4,3,0
+  """
+
   test "part one example" do
+    assert "4,6,3,5,6,3,5,2,1,0" == solve(@input, :part_one)
+  end
+
+  @part_one_solution "6,0,6,3,0,2,3,1,6"
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2024, 17, :part_one)
+  end
+
+  test "part two example" do
     input = ~S"""
-    Register A: 729
+    Register A: 2024
     Register B: 0
     Register C: 0
 
-    Program: 0,1,5,4,3,0
+    Program: 0,3,5,4,3,0
     """
 
-    assert "4,6,3,5,6,3,5,2,1,0" == solve(input, :part_one)
+    assert 117_440 == solve(input, :part_two)
   end
-
-  # Once your part one was successfully sumbitted, you may uncomment this test
-  # to ensure your implementation was not altered when you implement part two.
-
-  # @part_one_solution CHANGE_ME
-  #
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2024, 17, :part_one)
-  # end
-
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
 
   # You may also implement a test to validate the part two to ensure that you
   # did not broke your shared modules when implementing another problem.
